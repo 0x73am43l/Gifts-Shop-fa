7619c734cdd072e5e9a4539c14ae82ee0cbe9b0c
0x73am43l <0x73am43l@protonmail.ch>
2021-12-04 12:58:17 +0330

    add swiper script

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,38 @@
-const scrollProgress = document.getElementById('scroll-progress');
-const height =
-  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// const scrollProgress = document.getElementById('scroll-progress');
+// const height =
+//   document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-window.addEventListener('scroll', () => {
-  const scrollTop =
-    document.body.scrollTop || document.documentElement.scrollTop;
-  scrollProgress.style.width = `${(scrollTop / height) * 70}%`;
+// window.addEventListener('scroll', () => {
+//   const scrollTop =
+//     document.body.scrollTop || document.documentElement.scrollTop;
+//   scrollProgress.style.width = `${(scrollTop / height) * 70}%`;
+// });
+var Swiper;
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  freeMode: true,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    500: {
+      slidesPerView: 1
+    },
+    700: {
+      slidesPerView: 1.5
+    }
+  }
 });
