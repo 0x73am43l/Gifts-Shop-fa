c70823f3f1c334928ab31bffd88fe4e717380f73
0x73am43l <0x73am43l@protonmail.ch>
2021-12-23 23:51:20 +0330

    Update url

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import Home
+from .views import (
+    Home,
+    Support,
+    Privacy,
+    Terms,
+)
 
 urlpatterns = [
-    path('', Home, name='Home')
+    path('', Home, name='Home'),
+    path('support/', Support, name='support'),
+    path('privacy/', Privacy, name='privacy'),
+    path('terms-and-conditions/', Terms, name='terms')
 ]
