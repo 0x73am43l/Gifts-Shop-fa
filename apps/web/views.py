851c396f9239630220914c697186682ef632dab9
0x73am43l <0x73am43l@protonmail.ch>
2022-01-01 22:01:11 +0330

    Add: views

--- apps/web/views.py
+++ apps/web/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'index.html')
+
+def Support(request):
+    return render(request, 'support/support.html')
+
+def Privacy(request):
+    return render(request, 'support/privacy.html')
+
+def Terms(request):
+    return render(request, 'support/terms.html')
+
+def Contact_Us(request):
+    return render(request, 'support/contact_us.html')
