1c721a343756f8f049106da5cc5973e9533e4fd8
0x73am43l <0x73am43l@protonmail.ch>
2022-02-28 23:39:00 +0330

    Add: Account Admin Panel Fields

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-
 from apps.account.models import MyUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(MyUser)
+
+# UserAdmin.
+
+
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    fieldsets = (
+        ('اطلاعات کاربری', {
+            'fields': (
+                'phone', 'username', 'email', 'otp', 'otp_create_time', 'is_active', 'password'
+            )}),
+        ('دسترسی', {
+            'fields': (
+                ('is_superuser', 'is_staff', 'groups')
+
+            )}),
+    )
+    readonly_fields = ['otp_create_time']
+    list_display = ('phone', 'username', 'email', 'is_active')
+    ordering = ['pk']
+
+
+admin.site.register(MyUser, MyUserAdmin)
