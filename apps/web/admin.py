3a7697481655316044936dfce040d0bfea9ad8ad
0x73am43l <0x73am43l@protonmail.ch>
2022-03-05 19:02:24 +0330

    Update & Add: Custom Web Admin

--- apps/web/admin.py
+++ apps/web/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import SiteSetting, Slider, SocialMedia, Footer
 
-# Register your models here.
+
+class SiteSettingAdmin(admin.ModelAdmin):
+    list_display = ('site_title', 'logo',)
+
+
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+
+class SocialMediaAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+
+class FooterAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+
+admin.site.register(SiteSetting, SiteSettingAdmin)
+admin.site.register(Slider, SliderAdmin)
+admin.site.register(SocialMedia, SocialMediaAdmin)
+admin.site.register(Footer, FooterAdmin)
+
